4349cf7d30fec965568b82dfbf3f413f4597d59e
DiaAWAY <sindrerh@hotmail.com>
2011-03-22 15:56:21 +0100

    derp

--- TDT4240_project/src/org/group20/sunstruck/world/map/segments/MapSegment2.java
+++ TDT4240_project/src/org/group20/sunstruck/world/map/segments/MapSegment2.java
@@ -1,12 +1,9 @@
 package org.group20.sunstruck.world.map.segments;
 
-import com.badlogic.gdx.graphics.Texture;
 
 public class MapSegment2 extends MapSegment {
-
-	@Override
-	public Texture getTexture() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public MapSegment2() {
+		// initialize texture
 	}
 }
